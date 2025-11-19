2f906825b36e0eca55661d8ea5c0827a5347ce7a
fiyinfholuwa <fiyinfholuwa@gmail.com>
2025-11-19 13:59:36 +0100

    category c

--- resources/views/site/widgets/categories.blade.php
+++ resources/views/site/widgets/categories.blade.php
@@ -2,7 +2,8 @@
     <h3 class="widget-title">{{ __('categories') }}</h3>
     <ul class="global-list">
         @foreach($content as $item )
-            <li><a href="{{ url('category',$item->slug) }}">{{ $item->category_name }} <span>({{ $item->post_count }})</span></a></li>
+            {{-- <li><a href="{{ url('category',$item->slug) }}">{{ $item->category_name }} <span>({{ $item->post_count }})</span></a></li> --}}
+            <li><a href="{{ url('category',$item->slug) }}">{{ $item->category_name }} </span></a></li>
         @endforeach
     </ul>
 </div>
