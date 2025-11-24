cf295e97885e5e70f892573f47f467eb0f9d488c
fiyinfholuwa <fiyinfholuwa@gmail.com>
2025-11-24 17:26:04 +0100

    fixes

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -1,3 +1,20 @@
+
+
+<!-- Google AdSense Script -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5263422114514132"
+     crossorigin="anonymous"></script>
+
+<!-- Fluid Responsive Ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-i8-o+l-4h+dj"
+     data-ad-client="ca-pub-5263422114514132"
+     data-ad-slot="1645527264"></ins>
+
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 @php
 $footerWidgets = data_get($widgets, \Modules\Widget\Enums\WidgetLocation::FOOTER, []);
 @endphp
